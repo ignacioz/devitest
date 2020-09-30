26f5c1ee546db457103eb1e3a405253d6ca1d71b
Ignacio Zunino <zignacio@gmail.com>
2020-09-30 14:00:26 -0300

    added missing viewmodel file

---
+++ DevigetTest/ViewModels/RedditTableViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  RedditTableViewModel.swift
+//  DevigetTest
+//
+//  Created by Ignacio Zunino on 30/09/2020.
+//
+
+import Foundation
+
+final class RedditTableViewModel {
+    
+    private let redditService = RedditService()
+    
+    var items: [RedditItem] = []
+    
+    var reloadAction: (() -> Void)!
+    var itemRemoved: ((Int) -> Void)!
+
+    func loadData(done: (() -> Void)? = nil) {
+        redditService.fetchTop { [weak self] (response) in
+            self?.items = response.items
+            self?.reloadAction()
+            done?()
+        }
+    }
+    
+    func loadMore(done: (() -> Void)? = nil) {
+        guard let lastItem = items.last else {
+            return
+        }
+        redditService.fetchTop(after: lastItem) {[weak self] (response) in
+            self?.items.append(contentsOf: response.items)
+            self?.reloadAction()
+            done?()
+        }
+    }
+    
+    func removeItem(item: RedditItem) {
+        if let index = self.items.firstIndex(of: item) {
+            self.items.remove(at: index)
+            itemRemoved(index)
+        }
+    }
+    
+}
